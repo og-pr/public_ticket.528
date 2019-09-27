51f39ce6b2bd930e9f3c8ef0cdb44938273c692b
Otto Grajeda <ottograjeda@gmail.com>
2019-09-27 10:53:16 -0700

    Update

---
+++ SimpleApp/index.web.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import App from './app/components/App/'; // new = part of monorepo
+//import App from './app/web/'; // old = uses old folder structure where 'web' is not part of monorepo
+
+// Render is @ App = not needed here
+//ReactDOM.render(<App/>, document.getElementById('root')); // is loaded @ app/web/index.js
