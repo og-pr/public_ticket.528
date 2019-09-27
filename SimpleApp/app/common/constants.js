d0f265029c76d44a759d413cb00ff5006ee71c8f
Otto Grajeda <ottograjeda@gmail.com>
2019-09-27 11:48:39 -0700

    Update

---
+++ SimpleApp/app/common/constants.js
@@ -0,0 +1,35 @@
+// constants
+export const TEXT_THANKS = 'Thanks'
+export const TEXT_ENTER = '  Enter  ' // 2 blank spaces
+export const TEXT_REMOVE = '   Remove   ' // 3 blank spaces
+export const TEXT_ITEM_ADD = 'Add Item'
+export const TEXT_ITEM_LIST = 'List Item'
+export const TEXT_LOGO_TYPE = 'FOOD & FRIENDS'
+export const TEXT_FOOD = 'What food will you bring ?'
+//export const TEXT_LOGGED_OUT = 'You are logged out.'
+export const TEXT_CHOOSE = 'Please make a choice ; add or list.'
+//export const TEXT_LOGGED_IN = 'You must be logged in to see the list or add to it...'
+
+export const TEXT_CLEAR = ' '
+export const A_RETURN = 'return'
+export const NO_USER = 'no user'
+export const TEXT_LOGIN = 'Login'
+export const AN_ERROR = 'an error'
+export const TEXT_LOGOUT = 'Logout'
+export const TEXT_IN = 'SHOW LOGIN'
+export const TEXT_OUT = 'SHOW LOGOUT'
+
+export const TEXT_LOGIN_CAPS = 'LOGIN'
+export const TEXT_LOGOUT_CAPS = 'LOGOUT'
+
+// from SimpleRouting
+const PAGE_1 = '** Add **'
+const PAGE_2 = '** List **'
+const PAGE_3 = '** Logout **'
+
+export const PAGE_1_LINK = 'ADD' // space to center text in css block
+export const PAGE_2_LINK = 'LIST' // space to center text in css block
+export const PAGE_3_LINK = 'LOGOUT'
+//export const PAGE_3_TITLE = 'You are logged out.'
+
+//export const NOT_FOUND = 'Sorry... not found' // og.note: mechanism for missing routes not coded. left as exercise to repo user
